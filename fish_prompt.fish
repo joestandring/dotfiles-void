f8b4b1088be5856fdb8f8189dbe0a48878ae6267
Joe Standring <31478850+joestandring@users.noreply.github.com>
2019-03-25 07:14:43 +0000

    Delete fish_prompt.fish

--- fish_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_prompt
-	test $SSH_TTY
-    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
-    test "$USER" = 'root'
-    and echo (set_color red)"#"
-
-    # Main
-    echo -n (set_color red)'['(set_color brown)(date +%H)(set_color red)':'(set_color brown)(date +%M)(set_color red)':'(set_color brown)(date +%S)(set_color red)']'(set_color yellow)(whoami)(set_color green)'@'(set_color cyan)(prompt_hostname)(set_color blue) (prompt_pwd) (set_color purple)'❯' (set_color normal) 
-end
