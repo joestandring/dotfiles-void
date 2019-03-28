ada78a8f7796fdf785dbd0807b330537f08f66b5
Joe Standring <31478850+joestandring@users.noreply.github.com>
2019-03-28 14:04:04 +0000

    removed colour definitions

--- dwm/config.h
+++ dwm/config.h
@@ -8,11 +8,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack-Regular:size=14" };
 static const char dmenufont[]       = "Hack-Regular:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
 #include "/home/joe/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
